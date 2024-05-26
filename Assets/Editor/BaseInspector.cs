2db3c017fafb01459142d6bcb0f3cbbd3d33968e
GeorgeQuan <153918954+GeorgeQuan@users.noreply.github.com>
2024-05-26 23:45:57 +0800

    ProcedureModule增加注释

--- Assets/Editor/BaseInspector.cs
+++ Assets/Editor/BaseInspector.cs
@@ -1,53 +1,56 @@
+
 using UnityEditor;
 
-namespace TGame.Editor.Inspector
+
+public class BaseInspector :Editor
 {
-   
-	public class BaseInspector : UnityEditor.Editor
+    protected virtual bool DrawBaseGUI { get { return true; } }//决定是否绘制基础 GUI
+
+    private bool isCompiling = false;//当前编译状态
+    protected virtual void OnInspectorUpdateInEditor() { }//Update中方法 可以重写
+
+    private void OnEnable()
     {
-        protected virtual bool DrawBaseGUI { get { return true; } }
-
-        private bool isCompiling = false;
-        protected virtual void OnInspectorUpdateInEditor() { }
-
-        private void OnEnable()
-        {
-            OnInspectorEnable();
-            EditorApplication.update += UpdateEditor;
-        }
-        protected virtual void OnInspectorEnable() { }
-
-        private void OnDisable()
-        {
-            EditorApplication.update -= UpdateEditor;
-            OnInspectorDisable();
-        }
-        protected virtual void OnInspectorDisable() { }
-
-        private void UpdateEditor()
-        {
-            if (!isCompiling && EditorApplication.isCompiling)
-            {
-                isCompiling = true;
-                OnCompileStart();
-            }
-            else if (isCompiling && !EditorApplication.isCompiling)
-            {
-                isCompiling = false;
-                OnCompileComplete();
-            }
-            OnInspectorUpdateInEditor();
-        }
-
-        public override void OnInspectorGUI()
-        {
-            if (DrawBaseGUI)
-            {
-                base.OnInspectorGUI();
-            }
-        }
-
-        protected virtual void OnCompileStart() { }
-        protected virtual void OnCompileComplete() { }
+        OnInspectorEnable();//调用进入方法
+        EditorApplication.update += UpdateEditor;//Editor下的Update事件,每帧会调用,进入时添加update方法
     }
+    protected virtual void OnInspectorEnable() { }//进入
+    /// <summary>
+    /// 禁用时注销事件调用结束方法
+    /// </summary>
+    private void OnDisable()
+    {
+        EditorApplication.update -= UpdateEditor;
+        OnInspectorDisable();
+    }
+    protected virtual void OnInspectorDisable() { }
+    /// <summary>
+    /// Update事件
+    /// </summary>
+
+    private void UpdateEditor()
+    {
+        if (!isCompiling && EditorApplication.isCompiling)//EditorApplication.isCompiling 返回unity 是否在编译脚本 ,更改字段
+        {
+            isCompiling = true;
+            OnCompileStart();//调用刚开始编译脚本方法
+        }
+        else if (isCompiling && !EditorApplication.isCompiling)//停止编译脚本,更改字段
+        {
+            isCompiling = false;
+            OnCompileComplete();//调用编译完成方法
+        }
+        OnInspectorUpdateInEditor();//每帧调用update方法
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (DrawBaseGUI)//判断是否要绘制GUI
+        {
+            base.OnInspectorGUI();
+        }
+    }
+
+    protected virtual void OnCompileStart() { }
+    protected virtual void OnCompileComplete() { }
 }
--- Assets/Editor/ProcedureModuleInspector.cs
+++ Assets/Editor/ProcedureModuleInspector.cs
@@ -1,149 +1,162 @@
 using System;
 using System.Collections.Generic;
-using TGame.Common;
 using UnityEditor;
 using UnityEngine;
 
-namespace TGame.Editor.Inspector
+
+
+[CustomEditor(typeof(ProcedureModule))]//设置要编辑的类型
+public class ProcedureModuleInspector : BaseInspector
 {
-  
-    [CustomEditor(typeof(ProcedureModule))]
-    public class ProcedureModuleInspector : BaseInspector
+    private SerializedProperty proceduresProperty;//存储程序名字
+    private SerializedProperty defaultProcedureProperty;//默认程序名字
+
+    private List<string> allProcedureTypes;//所有程序名称容器
+    /// <summary>
+    /// 进入
+    /// </summary>
+    protected override void OnInspectorEnable()
     {
-        private SerializedProperty proceduresProperty;
-        private SerializedProperty defaultProcedureProperty;
+        base.OnInspectorEnable();
+        proceduresProperty = serializedObject.FindProperty("proceduresNames");//从要编辑的对象中找到这个属性,返回值是序列化的属性
+        defaultProcedureProperty = serializedObject.FindProperty("defaultProcedureName");
 
-        private List<string> allProcedureTypes;
+        UpdateProcedures();
+    }
 
-        protected override void OnInspectorEnable()
+    protected override void OnCompileComplete()
+    {
+        base.OnCompileComplete();
+        UpdateProcedures();
+    }
+    /// <summary>
+    ///更新方法
+    /// </summary>
+    private void UpdateProcedures()
+    {
+        allProcedureTypes = Utility.Types.GetAllSubclasses(typeof(BaseProcedure), false, Utility.Types.GAME_CSHARP_ASSEMBLY).ConvertAll((Type t) => { return t.FullName; });//获取所有子类型,并获取转换成类型的名字
+
+        //移除不存在的procedure
+        for (int i = proceduresProperty.arraySize - 1; i >= 0; i--)//倒序遍历属性
         {
-            base.OnInspectorEnable();
-            proceduresProperty = serializedObject.FindProperty("proceduresNames");
-            defaultProcedureProperty = serializedObject.FindProperty("defaultProcedureName");
-
-            UpdateProcedures();
-        }
-
-        protected override void OnCompileComplete()
-        {
-            base.OnCompileComplete();
-            UpdateProcedures();
-        }
-
-        private void UpdateProcedures()
-        {
-            allProcedureTypes = Utility.Types.GetAllSubclasses(typeof(BaseProcedure), false, Utility.Types.GAME_CSHARP_ASSEMBLY).ConvertAll((Type t) => { return t.FullName; });
-
-            //移除不存在的procedure
-            for (int i = proceduresProperty.arraySize - 1; i >= 0; i--)
+            string procedureTypeName = proceduresProperty.GetArrayElementAtIndex(i).stringValue;//获取元素,获取字符串值
+            if (!allProcedureTypes.Contains(procedureTypeName))//判断有没有继承BaseProcedure
             {
-                string procedureTypeName = proceduresProperty.GetArrayElementAtIndex(i).stringValue;
-                if (!allProcedureTypes.Contains(procedureTypeName))
-                {
-                    proceduresProperty.DeleteArrayElementAtIndex(i);
-                }
+                proceduresProperty.DeleteArrayElementAtIndex(i);//删除数组中这个位置的元素
             }
-            serializedObject.ApplyModifiedProperties();
         }
-
-        public override void OnInspectorGUI()
+        serializedObject.ApplyModifiedProperties();//将对序列化对象所做的修改应用到实际的对象。
+    }
+    /// <summary>
+    /// 重写绘制GUI方法
+    /// </summary>
+    public override void OnInspectorGUI()
+    {
+        EditorGUI.BeginDisabledGroup(Application.isPlaying); // 在游戏运行时禁用组,是否正在运行
         {
-            EditorGUI.BeginDisabledGroup(Application.isPlaying);
+            if (allProcedureTypes.Count > 0)//判断模块数量是否大于0
             {
-                if (allProcedureTypes.Count > 0)
+                EditorGUILayout.BeginVertical(GUI.skin.box);//创建垂直布局,GUI.skin.box有背景有边框
                 {
-                    EditorGUILayout.BeginVertical(GUI.skin.box);
+                    for (int i = 0; i < allProcedureTypes.Count; i++)//遍历所有程序名字
                     {
-                        for (int i = 0; i < allProcedureTypes.Count; i++)
+                        GUI.changed = false;//显示重置
+                        int? index = FindProcedureTypeIndex(allProcedureTypes[i]);//.......
+                        bool selected = EditorGUILayout.ToggleLeft(allProcedureTypes[i], index.HasValue);
+                        if (GUI.changed)//判断当前是否有GUI控件的值发生变化
                         {
-                            GUI.changed = false;
-                            int? index = FindProcedureTypeIndex(allProcedureTypes[i]);
-                            bool selected = EditorGUILayout.ToggleLeft(allProcedureTypes[i], index.HasValue);
-                            if (GUI.changed)
+                            if (selected)
                             {
-                                if (selected)
-                                {
-                                    AddProcedure(allProcedureTypes[i]);
-                                }
-                                else
-                                {
-                                    RemoveProcedure(index.Value);
-                                }
+                                AddProcedure(allProcedureTypes[i]);
+                            }
+                            else
+                            {
+                                RemoveProcedure(index.Value);
                             }
                         }
                     }
-                    EditorGUILayout.EndVertical();
                 }
+                EditorGUILayout.EndVertical();
             }
-            EditorGUI.EndDisabledGroup();
+        }
+        EditorGUI.EndDisabledGroup();
 
-            if (proceduresProperty.arraySize == 0)
+        if (proceduresProperty.arraySize == 0)
+        {
+            if (allProcedureTypes.Count == 0)
             {
-                if (allProcedureTypes.Count == 0)
-                {
-                    EditorGUILayout.HelpBox("Can't find any procedure", UnityEditor.MessageType.Info);
-                }
-                else
-                {
-                    EditorGUILayout.HelpBox("Please select a procedure at least", UnityEditor.MessageType.Info);
-                }
+                EditorGUILayout.HelpBox("Can't find any procedure", UnityEditor.MessageType.Info);
             }
             else
             {
-                if (Application.isPlaying)
-                {
-                    //播放中显示当前状态
-                    EditorGUILayout.LabelField("Current Procedure", TGameFramework.Instance.GetModule<ProcedureModule>().CurrentProcedure?.GetType().FullName);
-                }
-                else
-                {
-                    //显示默认状态
-                    List<string> selectedProcedures = new List<string>();
-                    for (int i = 0; i < proceduresProperty.arraySize; i++)
-                    {
-                        selectedProcedures.Add(proceduresProperty.GetArrayElementAtIndex(i).stringValue);
-                    }
-                    selectedProcedures.Sort();
-                    int defaultProcedureIndex = selectedProcedures.IndexOf(defaultProcedureProperty.stringValue);
-                    defaultProcedureIndex = EditorGUILayout.Popup("Default Procedure", defaultProcedureIndex, selectedProcedures.ToArray());
-                    if (defaultProcedureIndex >= 0)
-                    {
-                        defaultProcedureProperty.stringValue = selectedProcedures[defaultProcedureIndex];
-                    }
-                }
+                EditorGUILayout.HelpBox("Please select a procedure at least", UnityEditor.MessageType.Info);
             }
-
-            serializedObject.ApplyModifiedProperties();
         }
-
-        private void AddProcedure(string procedureType)
+        else
         {
-            proceduresProperty.InsertArrayElementAtIndex(0);
-            proceduresProperty.GetArrayElementAtIndex(0).stringValue = procedureType;
-        }
-
-        private void RemoveProcedure(int index)
-        {
-            string procedureType = proceduresProperty.GetArrayElementAtIndex(index).stringValue;
-            if (procedureType == defaultProcedureProperty.stringValue)
+            if (Application.isPlaying)
             {
-                Debug.LogWarning("Can't remove default procedure");
-                return;
+                //播放中显示当前状态
+                EditorGUILayout.LabelField("Current Procedure", TGameFramework.Instance.GetModule<ProcedureModule>().CurrentProcedure?.GetType().FullName);
             }
-            proceduresProperty.DeleteArrayElementAtIndex(index);
-        }
-
-        private int? FindProcedureTypeIndex(string procedureType)
-        {
-            for (int i = 0; i < proceduresProperty.arraySize; i++)
+            else
             {
-                SerializedProperty p = proceduresProperty.GetArrayElementAtIndex(i);
-                if (p.stringValue == procedureType)
+                //显示默认状态
+                List<string> selectedProcedures = new List<string>();
+                for (int i = 0; i < proceduresProperty.arraySize; i++)
                 {
-                    return i;
+                    selectedProcedures.Add(proceduresProperty.GetArrayElementAtIndex(i).stringValue);
+                }
+                selectedProcedures.Sort();
+                int defaultProcedureIndex = selectedProcedures.IndexOf(defaultProcedureProperty.stringValue);
+                defaultProcedureIndex = EditorGUILayout.Popup("Default Procedure", defaultProcedureIndex, selectedProcedures.ToArray());
+                if (defaultProcedureIndex >= 0)
+                {
+                    defaultProcedureProperty.stringValue = selectedProcedures[defaultProcedureIndex];
                 }
             }
-            return null;
         }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+    /// <summary>
+    /// 添加程序
+    /// </summary>
+    /// <param name="procedureType"></param>
+    private void AddProcedure(string procedureType)
+    {
+        proceduresProperty.InsertArrayElementAtIndex(0);
+        proceduresProperty.GetArrayElementAtIndex(0).stringValue = procedureType;
+    }
+    /// <summary>
+    /// 移除程序
+    /// </summary>
+    /// <param name="index"></param>
+    private void RemoveProcedure(int index)
+    {
+        string procedureType = proceduresProperty.GetArrayElementAtIndex(index).stringValue;
+        if (procedureType == defaultProcedureProperty.stringValue)
+        {
+            Debug.LogWarning("Can't remove default procedure");
+            return;
+        }
+        proceduresProperty.DeleteArrayElementAtIndex(index);
+    }
+    /// <summary>
+    /// 查找并返回序列化属性中的下标
+    /// </summary>
+    /// <param name="procedureType"></param>
+    /// <returns></returns>
+    private int? FindProcedureTypeIndex(string procedureType)
+    {
+        for (int i = 0; i < proceduresProperty.arraySize; i++)
+        {
+            SerializedProperty p = proceduresProperty.GetArrayElementAtIndex(i);
+            if (p.stringValue == procedureType)
+            {
+                return i;
+            }
+        }
+        return null;
     }
 }
